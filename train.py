77b15fc3b44de5b261b0f8b05903ca19a68c0670
Chaitan2004 <srichaitan26@gmail.com>
2026-01-10 00:02:04 +0530

    EXP-02 Linear Regression with StandardScaler, split 80/20

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -11,50 +12,41 @@ DATASET_PATH = "dataset/winequality-red.csv"
 OUTPUT_DIR = "outputs"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-# Load dataset
 data = pd.read_csv(DATASET_PATH, sep=";")
 
-# Features and target
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
-# Experiment configuration
 MODEL_TYPE = "linear"
-USE_SCALER = False
+USE_SCALER = True
 TEST_SIZE = 0.2
 
-# Train-test split
+if USE_SCALER:
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=TEST_SIZE, random_state=42
 )
 
-# Model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Predictions
 y_pred = model.predict(X_test)
 
-# Metrics
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 print("MSE:", mse)
 print("R2:", r2)
 
-# Save model
 joblib.dump(model, f"{OUTPUT_DIR}/model.pkl")
 
-# Save results
 with open(f"{OUTPUT_DIR}/results.json", "w") as f:
-    json.dump(
-        {
-            "model": MODEL_TYPE,
-            "scaler": USE_SCALER,
-            "test_size": TEST_SIZE,
-            "mse": mse,
-            "r2": r2
-        },
-        f,
-        indent=4
-    )
+    json.dump({
+        "model": MODEL_TYPE,
+        "scaler": USE_SCALER,
+        "test_size": TEST_SIZE,
+        "mse": mse,
+        "r2": r2
+    }, f, indent=4)
